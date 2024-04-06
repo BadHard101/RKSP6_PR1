cfd83af176fde7605600cc2d38c846389a892e4b
BadHard <BadHard101@yandex.ru>
2024-04-06 15:28:49 +0300

    PR4 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import './App.css'
 import Header from "./components/Header/Header.jsx";
 import {Route, Routes} from "react-router-dom";
 import Home from "./components/Pages/Home.jsx";
-import Prac2 from "./components/Pages/Prac2/Prac2.jsx";
 import Prac1 from "./components/Pages/Prac1/Prac1.jsx";
+import Prac2 from "./components/Pages/Prac2/Prac2.jsx";
 import Prac3 from "./components/Pages/Prac3/Prac3.jsx";
+import Prac4 from "./components/Pages/Prac4/Prac4.jsx";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/prac1" element={<Prac1/>} />
             <Route path="/prac2" element={<Prac2/>} />
             <Route path="/prac3" element={<Prac3/>} />
+            <Route path="/prac4" element={<Prac4/>} />
         </Routes>
     </>
   )
---
+++ src/components/Pages/Prac4/Prac4.jsx
@@ -0,0 +1,11 @@
+import classes from './Prac4.module.css'
+
+export default function Prac4() {
+
+    return (
+        <div className={classes.prac3}>
+            <h1>РКСП - Практическая работа №4</h1>
+            <p>Приложение теперь работает <b>без перезагрузок страниц</b> при помощи "Link", в которые обернуты компоненты "Button"</p>
+        </div>
+    )
+}
